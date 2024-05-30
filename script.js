d9f11aea08e5118329e63d9f15bb4ffb3d57235b
flaviaadiniz <flaviaadiniz@gmail.com>
2024-05-30 16:22:20 -0300

    Remove JS script for alert message and add background color for h4 on abrigos.css

--- script.js
+++ script.js
@@ -4,9 +4,4 @@ function goBack() {
     window.history.back();
 }
 
-document.getElementById("submitButton").addEventListener("click", goBack);
-
-function goBack() {
-    alert("Cadastro realizado com sucesso!");
-}
   
