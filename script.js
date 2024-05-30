e2bc9c4a295ac76b99b0ec299ce4e1c629303b9d
flaviaadiniz <flaviaadiniz@gmail.com>
2024-05-30 16:18:03 -0300

    Add a personalized message when the submit button is clicked on abrigos.html

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ document.getElementById("goBackButton").addEventListener("click", goBack);
 function goBack() {
     window.history.back();
 }
+
+document.getElementById("submitButton").addEventListener("click", goBack);
+
+function goBack() {
+    alert("Cadastro realizado com sucesso!");
+}
+  
