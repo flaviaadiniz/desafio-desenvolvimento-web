ea84536ad05e39cfdcba9d5188058ece4fb5c385
flaviaadiniz <flaviaadiniz@gmail.com>
2024-05-31 15:56:48 -0300

    Add button on abrigos.html to scroll back to top of the page

--- script.js
+++ script.js
@@ -4,4 +4,22 @@ function goBack() {
     window.history.back();
 }
 
+ // Function to scroll to the top of the page
+ function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth" // Smooth scrolling behavior
+    });
+  }
+
+  // Show the button when scrolling down
+  window.onscroll = function() {scrollFunction()};
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("backToTopBtn").style.display = "block";
+    } else {
+      document.getElementById("backToTopBtn").style.display = "none";
+    }
+  }
   
