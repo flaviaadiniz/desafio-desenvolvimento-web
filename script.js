2c60ccdcaf4c2daa75e5a57ec8758f7602e9ff86
flaviaadiniz <flaviaadiniz@gmail.com>
2024-06-01 22:33:20 -0300

    Fix function that takes the user back to index.html and delete unused code

--- script.js
+++ script.js
@@ -1,25 +1,14 @@
 document.getElementById("goBackButton").addEventListener("click", goBack);
 
 function goBack() {
-    window.history.back();
+  window.location.href = 'index.html';
 }
 
- // Function to scroll to the top of the page
  function scrollToTop() {
     window.scrollTo({
       top: 0,
-      behavior: "smooth" // Smooth scrolling behavior
+      behavior: "smooth"
     });
   }
 
-  // Show the button when scrolling down
-  window.onscroll = function() {scrollFunction()};
-
-  function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      document.getElementById("backToTopBtn").style.display = "block";
-    } else {
-      document.getElementById("backToTopBtn").style.display = "none";
-    }
-  }
   
